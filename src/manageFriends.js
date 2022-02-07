4294a4a0dbb451ebf30bb4f247c38f6c4596e827
androidmini9x <androidmini92@gmail.com>
2022-02-07 17:29:52 +0300

    Impement the functions

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
-export function manageFriends(state, action) {
+export function manageFriends(state = { friends : [] }, action) {
   // your code here
+  switch(action.type) {
+    case "friends/add":
+      return {friends: [...state.friends, {...action.payload}] };
+    case "friends/remove":
+      const newFriends = state.friends.filter(f => f.id !== action.payload);
+      return {friends: newFriends};
+    default:
+      return state;
+  }
 }
